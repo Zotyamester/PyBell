f62dc6b12ac1d6825a31eb432362624bcf7bcb75
Zotyamester <szatmary.zoltan1222@gmail.com>
2020-09-26 12:12:51 +0200

    Use WTForms everywhere

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,36 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from flask_wtf.file import FileAllowed, FileRequired
+from wtforms import (BooleanField, FileField, PasswordField, RadioField,
+                     SelectField, StringField, SubmitField)
+from wtforms.validators import DataRequired, InputRequired
+
+ALLOWED_SOUND_EXTENSIONS = ['mp3', 'wav']
+ALLOWED_CONFIG_EXTENSIONS = ['xml', 'bellxml']
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Log In')
+
+class SoundUploadForm(FlaskForm):
+    file = FileField('Sound', validators=[FileRequired('No soundfile selected for uploading'), \
+        FileAllowed(ALLOWED_SOUND_EXTENSIONS, 'You can only upload soundfiles')])
+    submit = SubmitField('Upload')
+
+class ConfigUploadForm(FlaskForm):
+    file = FileField('Config', validators=[FileRequired('No configfile selected for uploading'), \
+        FileAllowed(ALLOWED_CONFIG_EXTENSIONS, 'You can only upload configfiles')])
+    submit = SubmitField('Upload')
+
+class ConfigManagerForm(FlaskForm):
+    config = SelectField('Config', validators=[InputRequired()])
+    submit = SubmitField('Select')
+
+class BellManagerForm(FlaskForm):
+    state = RadioField('State', choices=['Start', 'Stop', 'Restart'], validators=[InputRequired()])
+    submit = SubmitField('Ok')
+
+class PlayInstantForm(FlaskForm):
+    sound = SelectField('Sound', validators=[InputRequired()])
+    submit = SubmitField('Play')
--- app/routes.py
+++ app/routes.py
@@ -11,11 +11,10 @@ from werkzeug.utils import secure_filename
 from app import app
 from app import bell as b
 from app import db
-from app.forms import LoginForm
+from app.forms import (BellManagerForm, ConfigManagerForm, ConfigUploadForm,
+                       LoginForm, PlayInstantForm, SoundUploadForm)
 from app.models import User
 
-ALLOWED_SOUND_EXTENSIONS = {'mp3', 'wav'}
-ALLOWED_CONFIG_EXTENSIONS = {'xml', 'bellxml'}
 
 def list_all_files(dirname):
 	all_files = []
@@ -24,22 +23,14 @@ def list_all_files(dirname):
 			all_files.append(filename)
 	return all_files
 
-def allowed_file(filename, allowed_exts):
-	return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_exts
-
-def upload_file(request, allowed_exts, folder):
-	if 'file' not in request.files:
-		flash('No file part')
-		return redirect(url_for('upload'))
-	file = request.files['file']
-	if file.filename == '':
-		flash('No file selected for uploading')
-	elif file and allowed_file(file.filename, allowed_exts):
-		filename = secure_filename(file.filename)
-		file.save(os.path.join(app.config[folder], filename))
+def upload_file(form, folder):
+	if form.validate_on_submit():
+		filename = secure_filename(form.file.data.filename)
+		form.file.data.save(os.path.join(app.config[folder], filename))
 		flash('File "%s" uploaded' % filename)
 	else:
-		flash('Allowed file types are %s' % ', '.join(allowed_exts))
+		for error in form.file.errors:
+			flash(error)
 	return redirect(url_for('upload'))
 
 @app.route('/', methods=['GET', 'POST'])
@@ -82,57 +73,67 @@ def internal_error(error):
 @app.route('/upload', methods=['GET'])
 @login_required
 def upload():
-	return render_template('upload.html')
+	form1 = SoundUploadForm()
+	form2 = ConfigUploadForm()
+	return render_template('upload.html', form1=form1, form2=form2)
 
 @app.route('/upload_sound', methods=['POST'])
 @login_required
 def upload_sound():
-	return upload_file(request, ALLOWED_SOUND_EXTENSIONS, 'SOUND_FOLDER')
+	form = SoundUploadForm()
+	return upload_file(form, 'SOUND_FOLDER')
 
 @app.route('/upload_config', methods=['POST'])
 @login_required
 def upload_config():
-	return upload_file(request, ALLOWED_CONFIG_EXTENSIONS, 'CONFIG_FOLDER')
+	form = ConfigUploadForm()
+	return upload_file(form, 'CONFIG_FOLDER')
 
 @app.route('/manage_configs', methods=['GET', 'POST'])
 @login_required
 def manage_configs():
+	form = ConfigManagerForm()
 	if request.method == 'POST':
-		config_select = request.form['config_select']
-		filename = secure_filename(config_select)
+		filename = secure_filename(form.config.data)
 		app.config['CURRENT_CONFIG'] = filename
 		b.load_schedule(os.path.join(app.config['CONFIG_FOLDER'], app.config['CURRENT_CONFIG']))
 		flash('Config updated')
 		return redirect(request.url)
-	all_files = list_all_files(app.config['CONFIG_FOLDER'])
-	return render_template('config_manager.html', files=all_files, current_config=app.config['CURRENT_CONFIG'])
+	form.config.choices = list_all_files(app.config['CONFIG_FOLDER'])
+	return render_template('config_manager.html', form=form, current_config=app.config['CURRENT_CONFIG'])
 
-@app.route('/manage_bell', methods=['GET', 'POST'])
+@app.route('/manage_bell', methods=['GET'])
 @login_required
 def manage_bell():
-	if request.method == 'POST':
-		selected = request.form['state']
-		if selected == 'start':
-			b.bell_start()
-			flash('Bell started')
-		elif selected == 'stop':
-			b.bell_stop()
-			flash('Bell stopped')
-		elif selected == 'restart':
-			b.bell_stop()
-			b.bell_start()
-			flash('Bell restarted')
-		else:
-			flash('Selection error')
-		return redirect(request.url)
-	all_files = list_all_files(app.config['SOUND_FOLDER'])
-	return render_template('bell_manager.html', files=all_files, state=b.running)
+	form1 = BellManagerForm()
+	form2 = PlayInstantForm()
+	form2.sound.choices = list_all_files(app.config['SOUND_FOLDER'])
+	return render_template('bell_manager.html', form1=form1, form2=form2, state=b.running)
+
+@app.route('/manage_scheduler', methods=['POST'])
+@login_required
+def manage_scheduler():
+	form = BellManagerForm()
+	selected = form.state.data
+	if selected == 'Start':
+		b.bell_start()
+		flash('Bell started')
+	elif selected == 'Stop':
+		b.bell_stop()
+		flash('Bell stopped')
+	elif selected == 'Restart':
+		b.bell_stop()
+		b.bell_start()
+		flash('Bell restarted')
+	else:
+		flash('Selection error')
+	return redirect(url_for('manage_bell'))
 
 @app.route('/play_instant', methods=['POST'])
 @login_required
 def play_instant():
-	sound_select = request.form['sound_select']
-	filename = secure_filename(sound_select)
+	form = PlayInstantForm()
+	filename = secure_filename(form.sound.data)
 	b.play_instant(os.path.join(app.config['SOUND_FOLDER'], filename))
 	flash('Now playing "%s".' % filename)
 	return redirect(url_for('manage_bell'))
